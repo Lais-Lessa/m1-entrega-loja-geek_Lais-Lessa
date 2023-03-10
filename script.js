ac6e6e37c2d8fe661376a28a080d0743ccf5ced9
Lais Lessa <124070111+Lais-Lessa@users.noreply.github.com>
2023-03-10 14:04:21 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let itens = [
+    produto1 = {
+        image: './assets/img/painting/clock.jpg',
+        name: 'Smartwatch',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto2 = {
+        image: './assets/img/painting/gamepad.jpg',
+        name: 'Controle Playstation',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto3 = {
+        image: './assets/img/painting/personagem.jpg',
+        name: 'Robot',
+        price: 'R$100,00',
+        type: 'Painting'
+    },
+    produto4 = {
+        image: './assets/img/actions/animewoman.jpg',
+        name: 'Anime Woman',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    },
+    produto5 = {
+        image: './assets/img/actions/dragonballpersonagem.jpg',
+        name: 'Goku Super Saiyajin',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    },
+    produto6 = {
+        image: './assets/img/actions/starwarspersonagem.jpg',
+        name: 'Sr Yoda',
+        price: 'R$100,00',
+        type: 'Action Figures'
+    }
+];
