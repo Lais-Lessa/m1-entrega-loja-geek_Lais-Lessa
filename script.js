6d2866330cc1cb4efa80302e92cdec722a37acc0
Lais Lessa <124070111+Lais-Lessa@users.noreply.github.com>
2023-03-10 18:00:01 -0300

    Tarefa_Finalizada

--- script.js
+++ script.js
@@ -36,3 +36,61 @@ let itens = [
         type: 'Action Figures'
     }
 ];
+
+let listFigures = [];
+let listFrames = [];
+const capturaListaDeQuadros = document.querySelector("#quadros")
+const capturaListaDeFiguras = document.querySelector("#figuras")
+
+function separateItens(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].type == "Painting") {
+            listFrames.push(array[i])
+        } else if (array[i].type == "Action Figures") {
+            listFigures.push(array[i])
+        }
+    }
+}
+separateItens(itens)
+
+for (let i = 0; i < listFigures.length; i++) {
+
+    let elementoLiFiguras = document.createElement("li");
+    elementoLiFiguras.classList.add("cards");
+    let imagemFiguras = document.createElement("img");
+    imagemFiguras.classList.add("image_cards");
+    let nomeH3Figuras = document.createElement("h3");
+    let paragrafoPrecoFiguras = document.createElement("p");
+
+    imagemFiguras.setAttribute("src", listFigures[i].image);
+    nomeH3Figuras.innerText = `${listFigures[i].name}`;
+    paragrafoPrecoFiguras.innerText = `${listFigures[i].price}`;
+
+    elementoLiFiguras.appendChild(imagemFiguras);
+    elementoLiFiguras.appendChild(nomeH3Figuras);
+    elementoLiFiguras.appendChild(paragrafoPrecoFiguras);
+
+    capturaListaDeFiguras.appendChild(elementoLiFiguras);
+}
+
+for (let i = 0; listFrames.length; i++) {
+
+    let elementoLiQuadros = document.createElement("li");
+    elementoLiQuadros.classList.add("cards");
+    let imagemQuadros = document.createElement("img");
+    imagemQuadros.classList.add("image_cards");
+    let nomeH3Quadros = document.createElement("h3");
+    nomeH3Quadros.classList.add("titulo_h3");
+    let paragrafoPrecoQuadros = document.createElement("p");
+    paragrafoPrecoQuadros.classList.add("preco");
+
+    imagemQuadros.setAttribute("src", listFrames[i].image);
+    nomeH3Quadros.innerText = `${listFrames[i].name}`;
+    paragrafoPrecoQuadros.innerText = `${listFrames[i].price}`;
+
+    elementoLiQuadros.appendChild(imagemQuadros);
+    elementoLiQuadros.appendChild(nomeH3Quadros);
+    elementoLiQuadros.appendChild(paragrafoPrecoQuadros);
+
+    capturaListaDeQuadros.appendChild(elementoLiQuadros);
+}
